17eca3ce607bb0ff654fff8f6a9c770e46a6d368
Chandrakant <cadaats@gmail.com>
2022-06-12 08:22:56 -0500

    stopwatch initilization

--- CRD.Commision.Calculator.Console/Program.cs
+++ CRD.Commision.Calculator.Console/Program.cs
@@ -13,27 +13,10 @@ namespace CRD.Commision.Calculator.ConsoleApp
             
             long runTimeForLinear, runTimeForMaxParalleism, runTimeForParallelBatch, runTimeForSingleTrade = 0;
             string testDataFile = "TestData/MultipleTrades_Large.json";
-
-            /// 
-            /// Run Single Trade
-            /// 
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            await testRunner.RunSingle(
-                new TradeRequest(
-                    securityType: CRD.Commission.Calculator.Models.Enums.SecurityTypes.COM,
-                    price: 100,
-                    quantity: 100_000,
-                    tradeDate: DateTime.Now,
-                    transactionType: CRD.Commission.Calculator.Models.Enums.TransactionType.BUY
-                ));
-            stopwatch.Stop();
-            runTimeForSingleTrade = stopwatch.ElapsedMilliseconds;
-
-
             ///// 
             ///// Run Linear trades
             ///// 
-            stopwatch.Restart();
+            Stopwatch stopwatch = Stopwatch.StartNew();
             await testRunner.RunMultipleLinear(testDataFile);
             stopwatch.Stop();
             runTimeForLinear = stopwatch.ElapsedMilliseconds;
@@ -56,6 +39,21 @@ namespace CRD.Commision.Calculator.ConsoleApp
             await testRunner.RunMultipleTradesInParallel(testDataFile, batchCount);
             stopwatch.Stop();
             runTimeForParallelBatch = stopwatch.ElapsedMilliseconds;
+
+            /// 
+            /// Run Single Trade
+            /// 
+            stopwatch.Restart();
+            await testRunner.RunSingle(
+                new TradeRequest(
+                    securityType: CRD.Commission.Calculator.Models.Enums.SecurityTypes.COM,
+                    price: 100,
+                    quantity: 100_000,
+                    tradeDate: DateTime.Now,
+                    transactionType: CRD.Commission.Calculator.Models.Enums.TransactionType.BUY
+                ));
+            stopwatch.Stop();
+            runTimeForSingleTrade = stopwatch.ElapsedMilliseconds;
 
             System.Console.WriteLine($"Time taken to complete for Single Trade: {runTimeForSingleTrade} ms");
             System.Console.WriteLine($"Time taken to complete by Linear  method: {runTimeForLinear} ms");
